c38a2acad0b6933cac2dc4ce2bedb750d5876e1c
Martin Imobersteg <martin.imobersteg@gmail.com>
2020-12-22 11:06:38 +0100

    Using Stacks to Arrange Views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Martin Imobersteg on 22.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
